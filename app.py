cdb7c5f611d8dfeddba1bc3d7a09589f1ebd00eb
yaserfarook1 <98206606+yaserfarook1@users.noreply.github.com>
2025-09-18 12:58:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
 from fastapi import FastAPI
-from fastapi.responses import HTMLResponse
+from fastapi.responses import JSONResponse
 
 app = FastAPI()
 
-@app.get("/hello", response_class=HTMLResponse)
+@app.get("/hello")
 def hello():
-    return """
-    <p>Hello Farook, You can see your dashboard reports at 
-    <a href="http://localhost:5174/" target="_blank">Dashboard</a></p>
-    """
+    return JSONResponse(content={
+        "message": "Hello Farook, You can see your dashboard reports",
+        "dashboard_link": {
+            "url": "http://localhost:5174/",
+            "label": "Dashboard"
+        }
+    })
